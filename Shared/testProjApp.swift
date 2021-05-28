9ea3c138507d3e243b8c86d14da3ed9b27628733
Vitalifront <vitaliklopau@gmail.com>
2021-05-28 20:12:24 +0200

    Initial Commit

---
+++ Shared/testProjApp.swift
@@ -0,0 +1,17 @@
+//
+//  testProjApp.swift
+//  Shared
+//
+//  Created by Виталий Клопов on 28.05.21.
+//
+
+import SwiftUI
+
+@main
+struct testProjApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
